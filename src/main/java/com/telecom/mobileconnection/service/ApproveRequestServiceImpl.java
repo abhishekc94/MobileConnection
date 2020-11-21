1919155b5fbec7bae5a77f469940337eed4690ec
unknown <manisha9233@gmail.com>
2020-11-21 14:17:03 +0000

    fix:exception Handling

--- src/main/java/com/telecom/mobileconnection/service/ApproveRequestServiceImpl.java
+++ src/main/java/com/telecom/mobileconnection/service/ApproveRequestServiceImpl.java
@@ -7,7 +7,9 @@ import java.util.Optional;
 import com.telecom.mobileconnection.dto.ApproveRequestDTO;
 import com.telecom.mobileconnection.dto.ApproveResponseDTO;
 import com.telecom.mobileconnection.entity.Track;
+import com.telecom.mobileconnection.exception.InvalidTrackIdException;
 import com.telecom.mobileconnection.repository.TrackRepository;
+import com.telecom.mobileconnection.util.EMobileConnectionUtil;
 
 @Service
 public class ApproveRequestServiceImpl implements ApproveRequestService {
@@ -16,7 +18,7 @@ public class ApproveRequestServiceImpl implements ApproveRequestService {
 	TrackRepository trackRepository;	
 	
 	@Override
-	public ApproveResponseDTO approveRequestByAdmin(ApproveRequestDTO approveRequestDTO, Integer trackId) {
+	public ApproveResponseDTO approveRequestByAdmin(ApproveRequestDTO approveRequestDTO, Integer trackId) throws InvalidTrackIdException {
 		ApproveResponseDTO approveResponseDTO = new ApproveResponseDTO();
 		Optional<Track> trackRecord = trackRepository.findByTrackId(trackId);
 		if(trackRecord.isPresent()) {
@@ -24,6 +26,8 @@ public class ApproveRequestServiceImpl implements ApproveRequestService {
 			track.setTrackStatus(approveRequestDTO.getStatus());
 			track.setApproverComments(approveRequestDTO.getApproverComments());
 			trackRepository.save(track);
+		}else {
+			throw new InvalidTrackIdException(EMobileConnectionUtil.NO_TRACK_ID_FOUND);
 		}
 		approveResponseDTO.setMessage("Success");
 		approveResponseDTO.setStatusCode("200");
